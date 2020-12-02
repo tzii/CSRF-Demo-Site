e38f4ac2b96721a983f5b3b5c0a04076a6fefb6a
Vũ <tuanvundvn@gmail.com>
2020-12-02 10:27:06 +0700

    prevent csrf attack

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,17 +6,15 @@ const path = require("path");
 const dbPath = path.join(process.cwd(), "db/user.db");
 
 export default async function (req, res) {
-    if (req.method !== "POST") return res.json({ message: "Error" });
-    await sqlite
-        .open({ filename: dbPath, driver: sqlite3.Database })
-        .then((db) =>
-            db.get(
-                `SELECT * FROM USER WHERE username="${req.body.username}" and password="${req.body.password}"`
-            )
-        )
-        .then((data) => {
-            if (!data) return res.json({ message: "Login failed" });
-            res.setHeader("Set-Cookie", cookie.serialize("id", data.id));
-            res.json({ message: "Loggin successfully" });
-        });
+  if (req.method !== "POST") return res.json({ message: "Error" });
+  await sqlite
+    .open({ filename: dbPath, driver: sqlite3.Database })
+    .then((db) =>
+      db.get(`SELECT * FROM USER WHERE username="${req.body.username}" and password="${req.body.password}"`)
+    )
+    .then((data) => {
+      if (!data) return res.json({ message: "Login failed" });
+      res.setHeader("Set-Cookie", cookie.serialize("id", data.id, { sameSite: "strict" }));
+      res.json({ message: "Loggin successfully" });
+    });
 }
